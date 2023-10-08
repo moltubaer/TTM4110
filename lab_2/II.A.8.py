3b7a4e329d9814ce2283ddc6060850ba433a3376
Marcus Johannessen <marcus-j@online.no>
2023-10-08 13:01:40 +0200

    time to clean a little

---
+++ lab_2/II.A.8.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# qulity level over time
+# price over time
+# and the price vs qulity level
+
+# simulation time = 24 hours
+
+
+
+
+plt.title('task II.A.8')
+
+x = []
+y = []
+
+
