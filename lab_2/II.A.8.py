868f9babf6d3cbce325b6ba5fe98bd703a0527e5
Marcus Johannessen <marcus-j@online.no>
2023-10-08 14:50:57 +0200

    spline for curve and .ignore file

--- lab_2/II.A.8.py
+++ lab_2/II.A.8.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.interpolate import make_interp_spline
+import numpy as np
 
 # qulity level over time
 # price over time
@@ -14,8 +16,14 @@ df =pd.read_csv('output.csv')
 time = df['time']
 price = df['price']
 
+
+time_price_spline = make_interp_spline(time, price)
+
+time2 = np.linspace(time.min(), time.max(), 500)
+price2 = time_price_spline(time2)
+
 plt.figure(figsize=(10,6))
-plt.plot(time, price, marker='o', linestyle='-')
+plt.plot(time2, price2, marker='o', linestyle='-')
 plt.title('task II.A.8')
 plt.xlabel('Time')
 plt.ylabel('Price')
